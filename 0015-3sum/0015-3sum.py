4348a8bd087783c977a4645942e15d01d4bd4be6
Garam Jo <134453584+smi-23@users.noreply.github.com>
2023-10-25 17:03:49 +0900

    Time: 1056 ms (60.87%), Space: 20.3 MB (73.20%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,41 +1,26 @@
 from typing import List
-
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int])-> list[list[int]]:
+        nums.sort()
         result = []
-        nums.sort()  # 입력 리스트를 정렬
-
-        for i in range(len(nums) - 2):
-            # 중복된 요소 건너뛰기
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range(len(nums)-2):
+            if i >0 and nums[i] == nums[i-1]:
                 continue
-
             left = i + 1
-            right = len(nums) - 1
-
+            right = len(nums)-1
             while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
-
-                if current_sum == 0:
-                    result.append([nums[i], nums[left], nums[right]])
-
-                    # 중복된 요소 건너뛰기
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
                     left += 1
+                elif total > 0:
                     right -= 1
-                elif current_sum < 0:
-                    left += 1
                 else:
-                    right -= 1
-
+                    temp = [nums[i], nums[left], nums[right]]
+                    result.append(temp)
+                    while left < right and nums[left] == nums[left +1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right -1]:
+                        right -= 1
+                    left += 1
+                    right -= 1       
         return result
-
-nums = [-1, 0, 1, 2, -1, -4]
-
-solution = Solution()
-result = solution.threeSum(nums)
-print("result =", result)
