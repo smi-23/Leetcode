b1ed1f8842b86f79d414155e9831055cdb4ec3d8
Garam Jo <134453584+smi-23@users.noreply.github.com>
2023-10-26 22:29:35 +0900

    Time: 368 ms (66.73%), Space: 16.2 MB (48.64%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.py
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.py
@@ -1,5 +1,6 @@
+from typing import List
 class Solution:
-    def unequalTriplets(self, nums: List[int]) -> int:
+    def unequalTriplets(self, nums: List[int])->int:
         nums.sort()
         cnt = 0
         for i in range(len(nums)-2):
@@ -7,6 +8,16 @@ class Solution:
                 if nums[i] == nums[j]:
                     continue
                 for k in range(j+1, len(nums)):
-                    if 0 <= nums[i] < nums[j] < nums[k]:
+                    if nums[i] != nums[j] != nums[k]:
                         cnt += 1
         return cnt
+nums = [4,4,2,4,3,5,6]
+"""
+# nums = [4,4,2,4,3]  
+# nums = [1,2,3]       
+# nums = [1,1,1,1]   
+"""   
+
+solution = Solution()
+result = solution.unequalTriplets(nums)
+print(f"result = {result}")
