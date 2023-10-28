541a50c5a587a1a1271c055eff19179f573a9ab1
Garam Jo <134453584+smi-23@users.noreply.github.com>
2023-10-28 22:06:15 +0900

    Time: 338 ms (63.44%), Space: 16.2 MB (82.73%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
@@ -1,8 +1,13 @@
 from typing import List
 class Solution:
     def findNonMinOrMax(self, nums: List[int]) -> int:
-        nums.sort()
-        n = len(nums)
-        if n <= 2:
+        # nums.sort()
+        # n = len(nums)
+        # if n <= 2:
+        #     return -1
+        # return nums[1]
+
+
+        if len(nums)<=2:
             return -1
-        return nums[1]
+        return sorted(nums[:3])[1]
