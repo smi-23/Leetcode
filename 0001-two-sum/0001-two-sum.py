7d67ac7f16614e1cb98935167f413defaa11f760
Garam Jo <134453584+smi-23@users.noreply.github.com>
2023-11-11 10:32:40 +0900

    Time: 71 ms (47.19%), Space: 17.6 MB (36.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,12 +2,30 @@ from typing import List
 class Solution:
     def twoSum(self, nums: list[int], target: int)->list[int]:
         table = {}
-        for i, num in enumerate(nums):
-            complement = target - num
+        n = len(nums)
+
+        # for i, num in enumerate(nums):
+        #     complement = target - num
+        #     if complement in table:
+        #         return table[complement], i
+        #     else:
+        #         table[num] = i
+                
+        
+        ## enumerate를 안하고
+        for i in range(n):
+            complement = target - nums[i]
             if complement in table:
-                return table[complement], i
+                return [table[complement], i]
             else:
-                table[num] = i
+                table[nums[i]] = i
+
+        # ## 완탐
+        # for i in range(n-1):
+        #     for j in range(i+1, n):
+        #         if target == nums[i] + nums[j]:
+        #             return [i, j]
+            
 
 nums = [2, 7, 11, 15]
 target = 9
